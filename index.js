ddd0fb533892b105a4682ca7b63720f29ee123b6
Dan Kibaara <kibaara.gamedev@gmail.com>
2024-09-23 11:53:42 +0300

    Adds arithmetic functions

---
+++ index.js
@@ -0,0 +1,35 @@
+//declaration
+
+//functn add
+function add(a,b){
+    return(a + b)
+}
+//functn subtract
+function subtract(a,b){
+    return (a - b)
+}
+//functn multiply
+function multiply(a,b){
+    return (a * b)
+}
+
+function divide(a,b){
+    return (a/b)
+}
+
+//functn increment
+function increment(a){return (a +=1)}
+
+//decrement
+function decrement(a){return (a -=1)}
+
+
+//makeInt parse to int
+function makeInt(a){
+    return parseInt(a,10)
+}
+
+//preserveDecimal
+function  preserveDecimal(n){
+    return parseFloat(n); //parse to float
+}
